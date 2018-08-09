d17455604325af36224bede2df51f684f3d55c70
vijayalakshmi121997 <41734864+vijayalakshmi121997@users.noreply.github.com>
2018-08-09 11:05:52 +0530

    Create word.java

---
+++ word.java
@@ -0,0 +1,19 @@
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		String s = "hello world";
+ 
+		int count = 1;
+ 
+		for (int i = 0; i < s.length() - 1; i++)
+		{
+			if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
+			{
+				count++;
+ 
+			}
+		}
+ 		System.out.println("Number of words in a string = " + count);
+	}
+}
